727a3139c24702113761b881abaf005557ab52b7
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2026-02-08 19:39:09 +0900

    Main 디렉토리 정리 (#1)
    * main directory
    
    * axios Instance 생성
    
    * main.jsx BrowserRouter추가
    
    * BrowserRouter install
    
    * Main Route 추가
    
    * 초기 정리
    
    * git 규칙 정리

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+// axios 객체 생성 (기본url, 시간 제한, 헤더, 로컬스토리지)
+const axiosIns = axios.create({
+    baseURL : "http://127.0.0.1:9090/api/v1/",
+    timeout : 5000,
+    headers : {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    withCredentials : false 
+});
+
+// 요청 형식
+axiosIns.interceptors.request.use(
+    // 요청에 성공할 경우
+    (config) => { 
+        // 토큰 가져오기
+        const token = localStorage.getItem("accessToken");
+
+        // 로그인 상태일 시 token을 붙여 반환
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        // 로그인 상태가 아니라면 그냥 반환
+        return config;
+    },
+    
+    // 요청에 실패할 경우, 계속 error 객체 반환
+    (error) => { return Promise.reject(error); }
+);
+
+// 응답 형식
+axiosIns.interceptors.response.use(
+    // 응답에 성공할 경우
+    (response) => {
+        return response;
+    },
+
+    // 응답에 실패한 경우
+    (error) => {
+        // 실패 시 응답 로직은 추후 추가
+        return Promise.reject(error);
+    }
+);
+
+export default axiosIns; 
