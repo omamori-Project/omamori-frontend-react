fe748e093c158ced72d2e5b7171297f4fbe568f4
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-11 01:02:00 +0900

    update: Rest api

---
+++ src/api/auth.api.js
@@ -0,0 +1,46 @@
+// auth.api.js 회원관리 api
+import axiosIns from "./axiosInstance.js";
+
+// 회원가입
+export const auth = async(authData) => {
+    const response = await axiosIns.post('/auth/register', authData);
+    return response.data;
+};
+
+// 로그인
+export const login = async(loginData) => {
+    const response = await axiosIns.post('/auth/login', loginData);
+    return response.data;
+};
+
+// 로그아웃
+export const logout = async() => {
+    await axiosIns.get('/auth/logout');
+    return true;
+};
+
+// 내 정보 조회
+export const myPage = async() => {
+    const response = await axiosIns.get('/me');
+    return response.data;
+};
+
+// 내 정보 수정
+export const editMyPage = async(editData) => {
+    const response = await axiosIns.patch('/me', editData);
+    return response.data;
+};
+
+// 회원 탈퇴
+export const deleteMyPage = async(password) => {
+    await axiosIns.delete('/me', password);
+    return true;
+};
+
+// (google OAuth) 시작
+export const googleAuth = async() => {
+    const response = await axiosIns.get('/auth/google');
+    return response.data;
+}
+
+// 
---
+++ src/api/omamori.api.js
@@ -0,0 +1,1 @@
+// omamori.api.js
