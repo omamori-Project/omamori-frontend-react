09f3d995322d2151b156c236ed9f8a21495770b6
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-17 20:28:38 +0900

    feat: build MyPage layout with logout UI

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,65 @@
+// pages/MyPage.jsx
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+
+export default function MyPage() {
+    const { user, logout } = useAuth();
+
+    // 로그아웃 핸들러
+    const handleLogout = () => {
+        logout();
+        navigate("/"); // 로그아웃 시 메인으로 이동
+    };
+
+    // 회원탈퇴 핸들러
+    const handleDeleteAccount = async () => {
+        const confirmDelete = window.confirm("정말 탈퇴하시겠습니까?");
+        if (!confirmDelete) return;
+    };
+
+    return (
+        <div>
+
+            {/* 상단 유저 정보 영역 */}
+            <header>
+                <h1>{user?.name} 님</h1>
+                <Link to="/mypage/edit">회원정보 수정</Link>
+                <button onClick={handleLogout}>로그아웃</button>
+                <button onClick={handleDeleteAccount}>회원탈퇴</button>
+            </header>
+
+            <hr />
+
+            {/* 메뉴 영역 */}
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/my-omamori">
+                            내 오마모리
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/my-posts">
+                            오마모리 게시글
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="/bookmarks">
+                            북마크 게시글
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr />
+
+            {/* 행운 컬러 보기 */}
+            <section>
+                <button> 오늘의 행운 컬러 보기 </button>
+            </section>
+
+        </div>
+    );
+}
