b1ae3614b34c0fef88fb6bfe18887556fcd7291b
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-17 20:27:45 +0900

    feat: add MyPage and EditProfile routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import GlobalModal from "./components/modals/GlobalModal";
+import ProtectedRoute from './components/common/ProtectedRoute';
+import MyPage from './pages/MyPage';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
     <>
-    <GlobalModal />
+      <GlobalModal />
+
       <Routes>
         {/* 주소가 / 이면 Main페이지 컴포넌트 실행 */}
         <Route path="/" element={<Main />} />
+
+        {/* 마이페이지 실행 */}
+        <Route path="/mypage" element={
+          <ProtectedRoute>
+            <MyPage />
+          </ProtectedRoute>
+          }
+        />
+
+        {/* 프로필 수정 */}
+        <Route path="/mypage/edit" element={
+          <ProtectedRoute>
+            <EditProfile />
+          </ProtectedRoute>
+          }
+        />     
+
       </Routes>
     </>
   );
