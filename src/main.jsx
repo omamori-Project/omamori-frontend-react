69c96f0a8b605cec2dc0575528a2a296c11a5cb0
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-17 20:27:12 +0900

    feat: wrap App with AuthProvider for global authentication state

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ModalProvider } from "./context/ModalContext";
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ModalProvider>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </ModalProvider>
     </BrowserRouter>
   </StrictMode>,
