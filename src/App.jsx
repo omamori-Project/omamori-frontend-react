6debe9f4823aedd69976caa53c9cdb6b16b069ae
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-19 16:34:47 +0900

    feature : Added a Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import GlobalModal from "./components/modals/GlobalModal";
+import GlobalModal from "./components/common/GlobalModal";
 import ProtectedRoute from './components/common/ProtectedRoute';
 import MyPage from './pages/MyPage';
-import EditProfile from './pages/EditProfile';
+import OAuthSuccess from './pages/OAuthSuccess';
 
 function App() {
   return (
@@ -15,21 +15,16 @@ function App() {
         {/* 주소가 / 이면 Main페이지 컴포넌트 실행 */}
         <Route path="/" element={<Main />} />
 
+        {/* 구글 계정 로그인 */}
+        <Route path="/oauth/callback" element={<OAuthSuccess />} />
+
         {/* 마이페이지 실행 */}
         <Route path="/mypage" element={
           <ProtectedRoute>
             <MyPage />
           </ProtectedRoute>
           }
-        />
-
-        {/* 프로필 수정 */}
-        <Route path="/mypage/edit" element={
-          <ProtectedRoute>
-            <EditProfile />
-          </ProtectedRoute>
-          }
-        />     
+        />   
 
       </Routes>
     </>
