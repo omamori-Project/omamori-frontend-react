2d559bf78069328fa57a91807a17093be7c945b1
dayeon <dayeon2423004@g.yju.ac.kr>
2026-02-23 03:37:53 +0900

    feat: 오마모리 제작 페이지 편집 기능 및 UI 개선

---
+++ src/components/modals/OmamoriCreateModal.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { omamori } from "../../api/omamori.api";
+
+export default function OmamoriCreateModal() {
+    const [userTitle, setUserTitle] = useState("");
+    const navigate = useNavigate();
+
+    // 제출
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await omamori({ title : userTitle }); 
+            const newId = response.data.id;
+
+            // 오마모리 제작 이동
+            navigate(`/omamori/edit/${newId}`);
+        } catch (error) {
+        alert("제작 중 오류가 발생했습니다.");
+        }
+    }
+
+    return (
+        <div className="modal">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="오마모리 제목"
+                    value={userTitle}
+                    onChange={(e) => {setUserTitle(e.target.value)}}
+                />
+            
+                <button type="submit">제목 생성</button>
+            </form>
+        </div>
+    )
+}
